24eadafce9e5a1e465eb20e096dda8a5dc87ce10
Rdx-projects <zainijuttttyyy@gmail.com>
2024-10-23 16:00:57 +0500

    Add files via upload

--- Priyansh/commands/0tea.js
+++ Priyansh/commands/0tea.js
@@ -1,26 +1,26 @@
 const fs = require("fs");
 module.exports.config = {
-  name: "tea",
+	name: "tea",
     version: "1.0.1",
-  hasPermssion: 0,
-  credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭", 
-  description: "hihihihi",
-  commandCategory: "no prefix",
-  usages: "tea",
+	hasPermssion: 0,
+	credits: "रवि✯कुमार", 
+	description: "hihihihi",
+	commandCategory: "no prefix",
+	usages: "tea",
     cooldowns: 5, 
 };
 
 module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
-  var { threadID, messageID } = event;
-  if (event.body.indexOf("tea")==0 || event.body.indexOf("Tea")==0 || event.body.indexOf("Chai")==0 || event.body.indexOf("CHAI")==0) {
-    var msg = {
-        body: "Ye Lo Bby ☕",
-        attachment: fs.createReadStream(__dirname + `/noprefix/tea.mp4`)
-      }
-      api.sendMessage(msg, threadID, messageID);
+	var { threadID, messageID } = event;
+	if (event.body.indexOf("tea")==0 || event.body.indexOf("Tea")==0 || event.body.indexOf("Cha")==0 || event.body.indexOf("Chaye")==0) {
+		var msg = {
+				body: "☕ ये लो बाबू चाय पी लो ☕",
+				attachment: fs.createReadStream(__dirname + `/cache/Tea.gif`)
+			}
+			api.sendMessage(msg, threadID, messageID);
     api.setMessageReaction("🫖", event.messageID, (err) => {}, true)
-    }
-  }
-  module.exports.run = function({ api, event, client, __GLOBAL }) {
+		}
+	}
+	module.exports.run = function({ api, event, client, __GLOBAL }) {
 
-  }
+	}
